5acc46a8cafb32a92ab761e97698f90274681e47
Greg Holmes <git@greg.holmes.name>
2023-08-23 19:32:30 +0100

    Move getOrCreateRole into library file.  Add perms

--- commands/newgamechannel.js
+++ commands/newgamechannel.js
@@ -1,50 +1,45 @@
-import {ChannelType, PermissionsBitField, SlashCommandBuilder} from 'discord.js'
+import {ChannelType, PermissionFlagsBits, PermissionsBitField, SlashCommandBuilder} from 'discord.js'
+import {getOrCreateRole} from '../lib/roles.js'
 
 export default class NewGameChannel {
   data = new SlashCommandBuilder().setName('newgamechannel').
-        setDescription('Creates a new game channel and associated permissions').
-        addStringOption(option => option.
-            setName('name').
-            setDescription('The name of the game').
-            setRequired(true))
+      setDescription('Creates a new game channel and associated permissions').
+      addStringOption(option => option.
+          setName('name').
+          setDescription('The name of the game').
+          setRequired(true)).
+      setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels).
+      setDMPermission(false)
 
   execute = async (interaction) => {
-    await interaction.respond({content: 'Creating game channel, bear with me', ephemeral: true})
-    const gameName = interaction.options.getString('name')
-    if (!gameName) {
-      await interaction.respond('Name not provided')
-      return
-    }
-    let role = await interaction.guild.roles.cache.
-        find(role => role.name === gameName)
-    if (!role) {
-      role = await interaction.guild.roles.create({
-        name: gameName, permissionOverwrites: [],
+    try {
+      //Defer response
+      await interaction.respond({content: 'Creating game channel, bear with me', ephemeral: true})
+      //Get entered name, fail if not provided
+      const gameName = interaction.options.getString('name')
+      let role = await getOrCreateRole(interaction.guild, gameName)
+      let channel = await interaction.guild.channels.cache.find(channel => channel.name === gameName && channel.parent?.id === process.env.GAMECHAN_CATEGORY)
+      if (channel) {
+        await interaction.respond('Channel already exists, stopping')
+        return
+      }
+      await interaction.guild.channels.create({
+        name: gameName,
+        parent: process.env.GAMECHAN_CATEGORY,
+        type: ChannelType.GuildText,
+        permissionOverwrites: [
+          {
+            id: interaction.guild.id,
+            deny: [PermissionsBitField.Flags.ViewChannel],
+          }, {
+            id: role.id,
+            allow: [PermissionsBitField.Flags.ViewChannel],
+          }],
       })
+      await interaction.respond('Channel created')
+    } catch (error) {
+      console.error(`Error creating game channel : ${error}`)
+      interaction.respond("There was an error creating the game role and channel")
     }
-    if (!role) {
-      await interaction.respond('Unable to get or create role')
-      return
-    }
-    let channel = await interaction.guild.channels.cache.
-        find(channel => channel.name === gameName && channel.parent?.id === process.env.GAMECHAN_CATEGORY)
-    if (channel) {
-      await interaction.respond('Channel already exists, stopping')
-      return
-    }
-    await interaction.guild.channels.create({
-      name: gameName,
-      parent: process.env.GAMECHAN_CATEGORY,
-      type: ChannelType.GuildText,
-      permissionOverwrites: [
-        {
-          id: interaction.guild.id,
-          deny: [PermissionsBitField.Flags.ViewChannel],
-        }, {
-          id: role.id,
-          allow: [PermissionsBitField.Flags.ViewChannel],
-        }],
-    })
-    await interaction.respond('Channel created')
   }
 }
